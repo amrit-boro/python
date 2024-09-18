2cb63f3353ce6ef4d099c53f9334e984c298995d
Amrit Boro <email.amrit.boro@gmail.com>
2024-09-18 20:23:57 +0530

    selection sort

--- index.py
+++ index.py
@@ -3,4 +3,4 @@ import sortingAlgo;
 
 # Example usage
 array = [64, 34, 25, 12, 22, 11, 90]
-print("Sorted array:", sortingAlgo.bubble_sort(array))
+print("Sorted array:", sortingAlgo.selection_sort(array))
--- sortingAlgo.py
+++ sortingAlgo.py
@@ -13,3 +13,19 @@ def bubble_sort(arr):
         if not swapped:
             break
     return arr
+
+
+def selection_sort(arr):
+    n = len(arr)
+    # Traverse through all array elements
+    for i in range(n):
+        # Find the minimum element in the remaining unsorted array
+        min_idx = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        
+        # Swap the found minimum element with the first element
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    
+    return arr
